76990fb2395b8f8b1cd0fc613df150b17c400f55
DuduMProg <edudu.dev@gmail.com>
2026-04-28 13:53:31 -0300

    "pouco a pouco estou trabalhando na category..."

--- database/migrations/2026_04_28_123321_add_category_id_to_products_table.php
+++ database/migrations/2026_04_28_123321_add_category_id_to_products_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->String('name');
-            $table->String('description');
-            $table->double('price');
-            $table->integer('category_id');
-            $table->timestamps();
+        Schema::table('products', function (Blueprint $table) {
+            //
         });
     }
 
@@ -26,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
     }
 };
