b2531e1e0b60d4c23db1e7d6bf7a97152bfc07e1
DuduMProg <edudu.dev@gmail.com>
2026-05-04 01:41:16 -0300

    "criacao das tags dos discos"

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -33,6 +33,7 @@
             <td><a href="/product/{{ $p->id }}">{{ $p->name }}</a></td>
             <td>{{ $p->artist }}</td>
             <td>{{ $p->category->name ?? 'Sem categoria' }}</td>
+            <td>{{ $p->tags ? $p->tags->count() : 0 }}</td>
             <td>{{ $p->stock }}</td>
             <td>R$ {{ number_format($p->price, 2, ',', '.') }}</td>
             <td>{{ $p->images_count }}</td>
